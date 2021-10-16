5a752555231bc14541f74be73b43cb09e3665a89
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-10-16 07:36:11 +0100

    form

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -3,18 +3,30 @@ import { useState } from "react";
 const CaughtPokemon = (props) => {
   let pokemons = ["pokemon0", "pokemon1", "pokemon2", "pokemon3","pokemon4"];
   let [Caught, setCaught] = useState([]);
+  let [pokemonNameInput, SetpokemonNAmeInput] = useState(null);
+  const handleInputChange = (e) => {
+    SetpokemonNAmeInput(e.target.value);
+
+  };
   const isCaught = () => {
-    let randomPokemon = Math.floor(Math.random() * pokemons.length);
-    setCaught(pokemons[randomPokemon]);
+   // let randomPokemon = Math.floor(Math.random() * pokemons.length);
+    //setCaught(pokemons[randomPokemon]);
+    setCaught(pokemons.concat(pokemonNameInput));
+    SetpokemonNAmeInput("");
+
  };
   return (
     <div>
       <p>Caught these Pokemon on {props.date}</p>
-      <button onClick={() =>isCaught()}>
-        Catch pokemon </button>
-    <ul>
+      <input
+        type="text"
+        value={pokemonNameInput}
+        onChange={(e) => handleInputChange(e)}
+      />
+      <button disabled={pokemonNameInput===" " ? "disabled" : null}onClick={() => isCaught()}>Catch pokemon </button>
+      <ul>
         <li> {Caught}</li>
-            </ul>
+      </ul>
     </div>
   );
 };
--- src/Logo.js
+++ src/Logo.js
@@ -5,7 +5,8 @@ const Logo = (props) => {
     <header>
       <h1> Welcome to Anza's {props.appName}</h1>
       <img
-        src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+        src="https://tse1.mm.bing.net/th?id=OIP.VHQdrIHAJC5YrzjRAvRBBgHaHa&pid=Api&P=0&w=300&h=300"
+        //https://assets.pokemon.com/assets/cms2/img/pokedex/full/038.png"
         alt="Pidgey"
         onClick={props.handleClick}
       ></img>
